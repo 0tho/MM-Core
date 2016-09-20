17885ee83a1d8c2aafb48905f20adf2d7e79fb4b
0tho <marcelo.lopes.lotufo@gmail.com>
2016-09-19 23:16:04 -0300

    Add a default resourcepack for configuration

--- src/main/java/me/otho/metamods/core/MmCore.java
+++ src/main/java/me/otho/metamods/core/MmCore.java
@@ -3,22 +3,29 @@ package me.otho.metamods.core;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import com.google.gson.JsonObject;
 
 import me.otho.metamods.core.jsonReader.JsonHandler;
 import me.otho.metamods.core.registry.RegisterHandler;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.FolderResourcePack;
+import net.minecraft.client.resources.IResourcePack;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.relauncher.ReflectionHelper;
 
-@Mod(modid = MmCore.MOD_ID, version = MmCore.VERSION)
+@Mod(modid = MmCore.MOD_ID, name = MmCore.MOD_NAME, version = MmCore.VERSION)
 public class MmCore
 {
     public static final String MOD_ID = "metamod-core";
+    public static final String MOD_NAME = "MetaMod - Core";
     public static final String VERSION = "0.0.0";
+    private static List<IResourcePack> defaultResourcePacks;
     
     private static File modConfigFolder;
     
@@ -29,6 +36,13 @@ public class MmCore
     public void preinit (FMLPreInitializationEvent  event)
     {
     	modConfigFolder = event.getModConfigurationDirectory();
+    	defaultResourcePacks = ReflectionHelper.getPrivateValue(Minecraft.class, Minecraft.getMinecraft(), "defaultResourcePacks", "field_110449_ao", "ap");
+    	String modResourcePackPath = modConfigFolder.toString() + File.separator + MmCore.MOD_ID + File.separator + "resources" + File.separator;
+    	File modResourceFile = new File(modResourcePackPath);
+    	if ( !modResourceFile.exists() ) {
+    		modResourceFile.mkdirs();
+    	}
+    	defaultResourcePacks.add(new FolderResourcePack( modResourceFile ));
     }    
     
     @EventHandler
@@ -36,19 +50,13 @@ public class MmCore
     {	
     	// Wait until init so other mods can interact with mm-core
     	// Get path of config files
-    	String modConfigPath = modConfigFolder.toString() + File.separator + MmCore.MOD_ID + File.separator;
-    	String jsonConfigPath = modConfigPath + "configs" + File.separator;
-    	// Mod Config Folder
-    	File configsFolder = new File(modConfigPath);
-    	// If it does not exist, make it
-    	if ( !configsFolder.exists()) {
-    		configsFolder.mkdir();
-    	}
+    	String jsonConfigPath = modConfigFolder.toString() + File.separator + MmCore.MOD_ID + File.separator + "configs" + File.separator;
+    	
     	// Get json folder
     	File jsonFolder = new File(jsonConfigPath); 
     	// Check if folder exists
     	if ( !jsonFolder.exists()) {
-    		jsonFolder.mkdir();
+    		jsonFolder.mkdirs();
     	} else {
     		// Read Json files from path
     		ArrayList<JsonObject> jsonData = JsonHandler.read( jsonFolder );
